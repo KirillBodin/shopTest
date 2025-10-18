4702f9c3da6f7a9e3fab1d4a2f091dc943272514
Your Name <you@example.com>
2025-10-18 14:53:31 +0000

    change corc

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,18 @@
+# config/initializers/cors.rb
 require "rack/cors"
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("CLIENT_ORIGIN", "*")
-    resource "*", headers: :any, methods: %i[get post put patch delete options head],
-                  expose: %w[Authorization]
+    allowed = ENV.fetch("CLIENT_ORIGINS", ENV.fetch("CLIENT_ORIGIN", "*"))
+                .split(",")
+                .map { _1.strip }
+
+    origins(*allowed)
+
+    resource "*",
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      expose: %w[Authorization],
+      credentials: true # важно, если фронт шлёт credentials: "include"
   end
 end
